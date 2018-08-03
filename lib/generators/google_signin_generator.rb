1fe077de25cf08641eeb2de7c82ed9e3506e5575
Daniel Buckle <danielbuckle31@gmail.com>
2018-08-03 16:56:30 +0100

    test

--- lib/generators/google_signin_generator.rb
+++ lib/generators/google_signin_generator.rb
@@ -2,5 +2,6 @@ class GoogleSigninGenerator < Rails::Generators::Base
   source_root(File.expand_path(File.dirname(__FILE__))
   def copy_initializer
     copy_file 'omniauth.rb', 'config/initializers/omniauth.rb'
+    puts "Install complete!"
   end
 end
