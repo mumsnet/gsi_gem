bd40624a408f3d26d5f90e89a2de18f475c8b9c4
Daniel Buckle <danielbuckle31@gmail.com>
2018-08-03 14:01:34 +0100

    update gem

--- lib/google_signin.rb
+++ lib/google_signin.rb
@@ -2,12 +2,6 @@ module GoogleSignin
   class << self
     require 'omniauth-google-oauth2'
 
-    def set_omniauth(client_id:, client_secret:)
-      Rails.application.config.middleware.use OmniAuth::Builder do
-        provider :google_oauth2, client_id, client_secret, { client_options: { ssl: { ca_file: Rails.root.join("cacert.pem").to_s }}}
-      end
-    end
-
     def authenticate_user(request)
       request.env['omniauth.auth']
     end
