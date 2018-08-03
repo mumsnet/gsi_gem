c991eca1ad6af67ed5cf22d68edf9e2fd119dd16
Daniel Buckle <danielbuckle31@gmail.com>
2018-08-03 11:31:11 +0100

    Fixes and add in initialiser

--- lib/google_signin.rb
+++ lib/google_signin.rb
@@ -2,15 +2,19 @@ module GoogleSignin
   class << self
     require 'omniauth-google-oauth2'
 
+    def set_omniauth(client_id:, client_secret:)
+      provider :google_oauth2, client_id, client_secret, { client_options: { ssl: { ca_file: Rails.root.join("cacert.pem").to_s }}}
+    end
+
     def authenticate_user(request)
       request.env['omniauth.auth']
     end
 
-    def create_session(user_id)
+    def create_session(session, user_id)
       session[:user_id] = user_id
     end
 
-    def destory_session
+    def destory_session(session)
       session[:user_id] = nil
     end
   end
