12c4dc27ba9014abaddcb54a41207622f730cf72
Daniel Buckle <danielbuckle31@gmail.com>
2018-08-06 14:43:52 +0100

    Fix

--- google_signin.gemspec
+++ google_signin.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'google_signin'
-  s.version = '1.0.8'
+  s.version = '1.0.9'
   s.date = '2018-08-02'
   s.summary = 'Authentication and Sign in with Google'
   s.description = 'Sends a user to the google login and returns a user object of who has moved in'
--- lib/generators/google_signin_generator.rb
+++ lib/generators/google_signin_generator.rb
@@ -1,7 +1,7 @@
 class GoogleSigninGenerator < Rails::Generators::Base
   source_root(File.expand_path(File.dirname(__FILE__))
   def copy_initializer
-    copy_file 'omniauth.rb', 'config/initializers/omniauth.rb'
+    template 'omniauth.rb', 'config/initializers/omniauth.rb'
     puts "Install complete!"
   end
 end
