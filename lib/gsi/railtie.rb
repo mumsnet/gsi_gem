8962e5deefef7058198382b91e42f8821dad40e2
Daniel Buckle <danielbuckle31@gmail.com>
2018-08-07 11:58:58 +0100

    Fix

--- lib/gsi/railtie.rb
+++ lib/gsi/railtie.rb
@@ -1,10 +1,12 @@
 require 'rails'
-class GSI::Railtie < Rails::Railtie
-  initializer 'my_railtie.configure_rails_initializer' do |app|
-    OmniAuth.config.logger = app.logger
+module GSI
+  class Railtie < Rails::Railtie
+    initializer 'my_railtie.configure_rails_initializer' do |app|
+      OmniAuth.config.logger = app.logger
 
-    app.config.middleware.use OmniAuth::Builder do
-      provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'], { client_options: { ssl: { ca_file: Rails.root.join("cacert.pem").to_s }}}
+      app.config.middleware.use OmniAuth::Builder do
+        provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'], { client_options: { ssl: { ca_file: Rails.root.join("cacert.pem").to_s }}}
+      end
     end
   end
 end
